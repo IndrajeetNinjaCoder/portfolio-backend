b309f5f5cbb06230492e6c09e201853b31ce193d
Indrajeet Kumar <2003indrajeet@gmail.com>
2026-04-16 00:12:43 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(express.json());
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // VERY IMPORTANT
+  port: 587,
+  secure: false, // IMPORTANT (false for 587)
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
